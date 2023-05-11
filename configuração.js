55b8628cf42cd825abeb762e808da582e7c07fa8
alizin-api <123191398+alizin-api@users.noreply.github.com>
2023-05-11 05:20:47 -0300

    Update configuração.js

--- configuração.js
+++ configuração.js
@@ -9,6 +9,6 @@ global.porta = process.env.PORT || 5000
 global.limitOfc = 3000
 global.tokens = 666
 global.limitPremium = 100000
-global.recaptcha_key_1 = '6LfNKRIlAAAAAJeABlLcScRJNId3tQf7qkOimZjc',
-global.recaptcha_key_2 = '6LfNKRIlAAAAAAc9jbMHryKJYjfT8zsgfdVzJGi5',
+global.recaptcha_key_1 = '6LdrAP4lAAAAAGQquAUTM9U2wZy1JFKxGKPz-V5O',
+global.recaptcha_key_2 = '6LdrAP4lAAAAACH_MBcjacxQm_izMejeRPl4ezn1',
 global.dbURI = 'mongodb+srv://MrRoots:Pietro0211@thecybers.hriy0ro.mongodb.net/?retryWrites=true&w=majority'
