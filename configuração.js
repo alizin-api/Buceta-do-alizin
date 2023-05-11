413657577a352ab0614ee957e5a629dd9ff4f50b
alizin-api <123191398+alizin-api@users.noreply.github.com>
2023-05-11 05:16:56 -0300

    Add files via upload

--- configuração.js
+++ configuração.js
@@ -1,1 +1,14 @@
-
+global.admin1 = 'alizindev'
+global.admin2 = 'admin'
+global.coderNumero = ['5521989701174']
+global.prefix = ['/']
+global.emoji_bo = 'alizim'
+global.auutor = 'alizim' //nome dos crÃ©ditos da figurinha > autor
+global.pacote = 'alizim' //nome dos crÃ©ditos da figurinha > pacote
+global.porta = process.env.PORT || 5000
+global.limitOfc = 3000
+global.tokens = 666
+global.limitPremium = 100000
+global.recaptcha_key_1 = '6LfNKRIlAAAAAJeABlLcScRJNId3tQf7qkOimZjc',
+global.recaptcha_key_2 = '6LfNKRIlAAAAAAc9jbMHryKJYjfT8zsgfdVzJGi5',
+global.dbURI = 'mongodb+srv://MrRoots:Pietro0211@thecybers.hriy0ro.mongodb.net/?retryWrites=true&w=majority'
